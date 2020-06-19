99c3720421953e8e02c1c289a44dcb2d70560496
Festus <festusowumi@gmail.com>
2020-06-19 03:52:41 +0100

    making progesss -- fixing up add  new tweet form

--- tweets/views.py
+++ tweets/views.py
@@ -1,26 +1,54 @@
+import random
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
-from django.http import HttpResponse,Http404,JsonResponse
 
+from .forms import TweetForm
 from .models import Tweet
 
-def home_view(request,*args,**kwargs):
-    return render(request,"pages/home.html",context={},status=200)
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    return render(request, "pages/home.html", context={}, status=200)
 
-def tweet_list_view(request,*arge,**kwargs):
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    print('post data is', request.POST)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        # do other form related logic
+        obj.save()
+        form = TweetForm()
+    return render(request, 'components/form.html', context={"form": form})
+
+
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Andriod
+    return json data
+    """
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id,"content":x.content} for x in qs]
+    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 122)} for x in qs]
     data = {
+        "isUser": False,
         "response": tweets_list
     }
     return JsonResponse(data)
 
-def tweet_detail_view(request,tweet_id,*args,**kwargs):
-    data = {"id": tweet_id,}
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Andriod
+    return json data
+    """
+    data = {
+        "id": tweet_id,
+    }
     status = 200
     try:
         obj = Tweet.objects.get(id=tweet_id)
-        data["content"] = obj.content
+        data['content'] = obj.content
     except:
-        data["message"] = "Not found"
+        data['message'] = "Not found"
         status = 404
-    return JsonResponse(data,status=status)
+    return JsonResponse(data, status=status) # json.dumps content_type='application/json'
